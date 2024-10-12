4b1bf452724f0858bb90d6a346b832056d97f2f2
rishav197 <rv1972002gmail.com>
2024-10-12 11:42:22 +0530

    one ques added

--- Heaps/kth-smallest.cpp
+++ Heaps/kth-smallest.cpp
@@ -5,23 +5,26 @@ class Solution {
     // arr : given array
     // k : find kth smallest element and return using this function
     int kthSmallest(vector<int> &arr, int k) {
-        priority_queue<int> pq;
         
-        //step1:
-        for(int i=0; i<k; i++){
-            pq.push(arr[i]);
+        int n = arr.size();
+        
+        //step1: build max heap for first K elements 
+        priority_queue<int> mxheap;
+        for(int idx=0; idx<k; idx++){
+            mxheap.push(arr[idx]);
         }
         
-        //step2
-        for(int i=k; i<arr.size(); i++){
-            if(arr[i]<pq.top()){
-                pq.pop();
-                pq.push(arr[i]);
+        //step2: for rest elements
+        for(int idx=k; idx<n; idx++){
+            int element = arr[idx];
+            
+            if(element<mxheap.top()){
+                mxheap.pop();
+                mxheap.push(element);
             }
         }
         
-        //ans = pq.top()
-        int ans = pq.top();
+        int ans = mxheap.top();
         return ans;
     }
 };
