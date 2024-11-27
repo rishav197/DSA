955906d25db77ffc2403e4bc7253b5e8254a12b7
rishav197 <rv1972002gmail.com>
2024-11-27 18:18:11 +0530

    one more ques added

---
+++ Graph/BFS-and-DFS/Cycle-detection-in-directed-graph-DFS.cpp
@@ -0,0 +1,53 @@
+#include<unordered_map>
+#include<list>
+
+bool checkCycleDFS(int node, unordered_map<int, bool> &visited, unordered_map<int, bool> &dfsVisited, unordered_map<int, list<int>> &adj){
+  visited[node]=true;
+  dfsVisited[node]=true;
+
+  for(auto neighbour : adj[node]){
+    if(!visited[neighbour]){
+      bool cycleDetected = checkCycleDFS(neighbour, visited, dfsVisited, adj);
+      if(cycleDetected){
+        return true;
+      }
+      
+    }
+    else if(dfsVisited[neighbour]){
+      //cycle present
+      return true;
+    }
+  }
+
+  dfsVisited[node]=false;
+  return false;
+}
+
+int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
+  
+  //create adj list
+  unordered_map<int, list<int>> adj;
+  for(int i=0; i<edges.size(); i++){
+    int u = edges[i].first;
+    int v = edges[i].second;
+
+    adj[u].push_back(v);
+  }
+
+  //call dfs for all components
+  unordered_map<int, bool> visited;
+  unordered_map<int, bool> dfsVisited;
+
+  for(int i=0; i<=n; i++){
+    if(!visited[i]){
+      bool cycleFound = checkCycleDFS(i, visited, dfsVisited, adj);
+
+      if(cycleFound==true){
+        return true;
+      }
+    }
+  }
+
+  return false;
+
+}
